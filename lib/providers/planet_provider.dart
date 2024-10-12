ed1440daef33211abe6225fdd8123f1ed7d81ae4
YudthanaMaingam <yudthanamaingam.2@gmail.com>
2024-10-12 13:44:33 +0700

    Add files via upload

--- lib/providers/planet_provider.dart
+++ lib/providers/planet_provider.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/foundation.dart';
+import 'package:project/database/planet_database.dart';
 import 'package:project/models/planet_detail.dart';
 
 class PlanetProvider with ChangeNotifier{
   List<Planet> planetList = [
-    Planet(name: "WASP-1", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2006", type: Type.bType, date: DateTime.now()),
-    Planet(name: "WASP-16", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2009", type: Type.dType, date: DateTime.now()),
-    Planet(name: "Gliese 581", discover: "Stéphane Udry Team", timeDiscover: "2007", type: Type.cType, date: DateTime.now()),
+    // Planet(name: "WASP-1", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2006", type: Type.bType, date: DateTime.now()),
+    // Planet(name: "WASP-16", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2009", type: Type.dType, date: DateTime.now()),
+    // Planet(name: "Gliese 581", discover: "Stéphane Udry Team", timeDiscover: "2007", type: Type.cType, date: DateTime.now()),
 
   ];
 
@@ -13,7 +14,9 @@ class PlanetProvider with ChangeNotifier{
     return planetList;
   }
 
-  void addPlanet(Planet statement){
+  void addPlanet(Planet statement) async {
+    var db = await PlanetDB(dbName: "planet.db").openDatabase();
+    print(db);
     planetList.insert(0,statement);
     //แจ้งเตือน Consumer
     notifyListeners();
