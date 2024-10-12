e6743cfae3839a78db95be1bf5e01342eaf0c23e
YudthanaMaingam <yudthanamaingam.2@gmail.com>
2024-10-12 17:26:41 +0700

    Add files via upload

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,8 @@
-import 'package:account/provider/transaction_provider.dart';
-import 'package:account/screens/edit_screen.dart';
-import 'package:account/screens/edit_screen.dart.bak';
-import 'package:account/screens/form_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:project/models/planet_detail.dart';
+import 'package:project/providers/planet_provider.dart';
+import 'package:project/screens/form_screen.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,70 +14,68 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  // void initState(){
+  //   // TODO: implement initState
+  //   super.initState();
+  //   Provider.of<PlanetProvider>(context, listen: false).initData();
+  // }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
           centerTitle: true,
-          title: const Text("My Planet",style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+          title: const Text(
+            "Planet",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
           actions: [
             IconButton(
-              icon: const Icon(Icons.exit_to_app),
-              onPressed: () {
-                SystemNavigator.pop();
-              },
-            ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const FormScreen();
+                  }));
+                },
+                icon: const Icon(Icons.add_outlined))
           ],
         ),
         body: Consumer(
-          builder: (context, TransactionProvider provider, Widget? child) {
-            if (provider.transactions.isEmpty) {
+          builder: (context, PlanetProvider provider, Widget? child) {
+            if(provider.planetList.isEmpty){
               return const Center(
-                child: Text('ไม่มีดาวเคราะห์'),
+                child: Text("ไม่พบข้อมูลดวงดาว", style: TextStyle(fontSize: 20),),
               );
             } else {
               return ListView.builder(
-                itemCount: provider.transactions.length,
-                itemBuilder: (context, index) {
-                  var statement = provider.transactions[index];
+                itemCount: provider.planetList.length,
+                itemBuilder: (context, int index) {
+                  Planet data = provider.planetList[index];
                   return Card(
+                    color: (data.type.color),
                     elevation: 5,
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                    margin: const EdgeInsets.symmetric(vertical: 11, horizontal: 10),
                     child: ListTile(
-                      title: Text(statement.title),
-                      subtitle: Text(DateFormat('dd MMM yyyy hh:mm:ss')
-                          .format(statement.date)),
                       leading: CircleAvatar(
-                        radius: 30,
+                        radius: 40,
                         child: FittedBox(
-                          child: Text('${statement.amount}'),
+                          child: Text(data.type.title)
                         ),
                       ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          provider.deleteTransaction(statement.keyID);
-                        },
+                      title: Text(data.name),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("ผู้ค้นพบ:  ${data.discover}"),
+                          Text("เวลาที่ค้นพบ: ค.ศ. ${data.timeDiscover}"),
+                          Text("ชนิด: ${data.type.title}"),
+                          Text("บันทึกเมื่อ: ${DateFormat("dd/MM/yyyy").format(data.date)}"),
+                        ],
                       ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return EditScreen(statement: statement);
-                            },
-                          ),
-                        );
-                      },
                     ),
                   );
-                },
-              );
+                });
             }
           },
-        )
-        // This trailing comma makes auto-formatting nicer for build methods.
-        );
+        ));
   }
 }
