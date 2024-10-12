7497fc8542006dc802831908159a7a976b1457a2
YudthanaMaingam <yudthanamaingam.2@gmail.com>
2024-10-12 20:53:50 +0700

    Add files via upload

--- lib/models/planet_detail.dart
+++ lib/models/planet_detail.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 enum Type {
   bType(title:"b", color: Color.fromARGB(255, 240, 196, 131)),
   dType(title:"d", color: Color.fromARGB(255, 242, 168, 255)),
-  cType(title: "c", color: Color.fromARGB(55, 247, 128, 128));
+  cType(title: "c", color: Color.fromARGB(255, 248, 135, 135)),
+  noneType(title: "None", color: Color.fromARGB(255, 255, 0, 0));
+  
 
   const Type({required this.title, required this.color});
   final String title;
@@ -12,11 +14,12 @@ enum Type {
 }
 
 class Planet {
+  int? keyID;
   String name; // ชื่อดาว
   String discover; // ผู้ค้นพบ
   String timeDiscover; // ปีที่ค้นพบ
   Type type; // ชนิด
   DateTime date; // วันเวลาที่บันทุึก
 
-  Planet({required this.name, required this.discover, required this.timeDiscover, required this.type, required this.date});
+  Planet({this.keyID,required this.name, required this.discover, required this.timeDiscover, required this.type, required this.date});
 }
