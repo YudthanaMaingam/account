5956265501b1b4864ed9614265bb92e8d04d7520
YudthanaMaingam <yudthanamaingam.2@gmail.com>
2024-10-12 15:40:06 +0700

    Add files via upload

--- lib/providers/planet_provider.dart
+++ lib/providers/planet_provider.dart
@@ -3,20 +3,17 @@ import 'package:project/database/planet_database.dart';
 import 'package:project/models/planet_detail.dart';
 
 class PlanetProvider with ChangeNotifier{
-  List<Planet> planetList = [
-    // Planet(name: "WASP-1", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2006", type: Type.bType, date: DateTime.now()),
-    // Planet(name: "WASP-16", discover: "Cameron et al.(SuperWASP and SOPHIE)", timeDiscover: "2009", type: Type.dType, date: DateTime.now()),
-    // Planet(name: "Gliese 581", discover: "Stéphane Udry Team", timeDiscover: "2007", type: Type.cType, date: DateTime.now()),
-
-  ];
+  List<Planet> planetList = [];
 
   List<Planet> getPlanet(){
     return planetList;
   }
 
   void addPlanet(Planet statement) async {
-    var db = await PlanetDB(dbName: "planet.db").openDatabase();
-    print(db);
+    var db = PlanetDB(dbName: "planet.db");
+    //save ข้อมูล
+    await db.InsertData(statement);
+
     planetList.insert(0,statement);
     //แจ้งเตือน Consumer
     notifyListeners();
