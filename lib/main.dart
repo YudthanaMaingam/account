0297a4c6f2574798933957a2aa9c45d86485e27a
YudthanaMaingam <yudthanamaingam.2@gmail.com>
2024-10-12 21:38:51 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:account/screens/form_screen.dart';
-import 'package:account/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+// import 'package:project/models/planet_detail.dart';
+import 'package:project/providers/planet_provider.dart';
+import 'package:project/screens/form_screen.dart';
+import 'package:project/screens/home_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:account/provider/transaction_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,29 +13,28 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) {
-          return TransactionProvider();
-        }),
-      ],
+      providers: [ChangeNotifierProvider(create: (context) {
+        return PlanetProvider();
+      })],
+
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: const MyHomePage(),
+        home: const MyHomePage(title: 'Planet'),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -41,29 +42,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  void initState() {
+
+  void initState(){
     // TODO: implement initState
     super.initState();
-    Provider.of<TransactionProvider>(context, listen: false).initData();
+    Provider.of<PlanetProvider>(context, listen: false).initData();
   }
 
   @override
   Widget build(BuildContext context) {
     return const DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: TabBarView(
-            children: [
-              HomeScreen(),
-              FormScreen(),
-            ],
-          ),
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body:TabBarView(
+          children: [
+            HomeScreen(),
+            FormScreen()
+          ]) ,
           bottomNavigationBar: TabBar(
-            tabs: [
-              Tab(text: "รายการธุรกรรม", icon: Icon(Icons.list),),
-              Tab(text: "เพิ่มข้อมูล", icon: Icon(Icons.add),),
-            ],
+            tabs:[
+              Tab(
+                icon: Icon(Icons.list_alt_outlined),
+                text: "รายการดวงดาว"
+                ),
+              Tab(
+                icon: Icon(Icons.add_box_outlined),
+                text: "เพิ่มดวงดาว"
+                )
+            ]),
           ),
-        ));
+          );
   }
 }
